201f7fc2d9bbc658c310ccfacc2f1b1250f5c602
apons <apons@APONS-HP-EMO>
2019-01-17 14:10:06 +0100

    test url

--- test.php
+++ test.php
@@ -20,7 +20,7 @@ fputs($fo, "Host: imaginator.omatech.com\r\n");
 fputs($fo, "Referer: http://www.omatech.com.com\r\n");
 fputs($fo, "User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)\r\n\r\n");
 echo "Error $errno: $errstr<br />";
-return count($fo);
+echo $fo;die;
 	/*
 	$arrContextOptions = array(
 		"ssl" => array(
