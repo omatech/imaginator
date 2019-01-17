0df4c5a9a63db674355c3f26cc83dbd1a8d3a116
apons <apons@APONS-HP-EMO>
2019-01-17 15:33:11 +0100

    test url

--- original.php
+++ original.php
@@ -13,9 +13,8 @@ require_once __DIR__.$autoload_location;
 $source_folder=$_SERVER["DOCUMENT_ROOT"].'/original_images';
 $cache_folder=$_SERVER["DOCUMENT_ROOT"].'/cache_images';
 $imaginator=new \Omatech\Imaginator\Imaginator($source_folder, $cache_folder);
-$url = parse_url($_SERVER['REQUEST_URI']);
-$parts=explode('/', $url);
-print_r($url);
+$parts=explode('/', $_SERVER['REQUEST_URI']);
+print_r($parts);
 $pathparts=pathinfo($url['path']);
 $filename=$pathparts['filename'];
 
